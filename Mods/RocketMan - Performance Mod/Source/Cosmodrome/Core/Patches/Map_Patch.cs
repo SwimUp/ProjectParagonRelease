76c4b90c9dfd116e883085ff6ad84303f22f2a3b
SwimUp <koviazin.pro@yandex.ru>
2024-01-24 17:53:26 +0500

    Project Paragon v0.5b
        Обновлен Giddy-Up! на Giddy-Up 2;
        Обновлен RocketMan;
    
        Изменен баланс зданий арахнидов;
        Многие арахниды потеряли защиту конечностей, что делает их более простой целью;
        Изменена скорость движения некоторых арахнидов в большую сторону;
        Арахнид переработчик больше не способен переварить древесину и уголь;
        Добавлена способность массового стана для арахнида-тарана;
        Исправления в генерации ксенотипов у рас;
        Новое существо - паровая корова, производящая коксовый уголь из органики;
        Поедание мяса насекомых не дает дебаффа при соответствующем принципе идеологии;
        Исправлена ошибка с инструментами;
        Исправлены ошибки при стрельбе;
        Баланс средств защиты неолита;
    
    Возможны красные ошибки при запуске сохранения, которые не должны быть критичными.
    
    Требуется обновление ModsConfig.

---
+++ Mods/RocketMan - Performance Mod/Source/Cosmodrome/Core/Patches/Map_Patch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace RocketMan.Patches
+{
+    [RocketPatch(typeof(Pawn_AbilityTracker), nameof(Pawn_AbilityTracker.ExposeData))]
+    public static class Pawn_AbilityTracker_ExposeData_Patch
+    {
+        public static Exception Finalizer(Pawn_AbilityTracker __instance, Exception __exception)
+        {
+            if (__exception != null && __instance.abilities == null)
+            {
+                Log.Warning($"RocketMan: Fixed loading Pawn_AbilityTracker for {__instance.pawn}");
+                __instance.abilities               = new List<Ability>();
+                __instance.allAbilitiesCachedDirty = true;
+            }
+            return null;
+        }
+    }
+    
+    [RocketStartupPatch(typeof(Map), nameof(Map.FinalizeInit))]
+    public static class Map_FinalizeInit_Patch
+    {
+        [HarmonyPriority(int.MinValue)]
+        public static void Postfix(Map __instance)
+        {
+            Main.MapLoaded(__instance);
+        }
+    }
+
+    [RocketStartupPatch(typeof(Map), nameof(Map.ConstructComponents))]
+    public static class Map_ConstructComponents_Patch
+    {
+        [HarmonyPriority(int.MinValue)]
+        public static void Postfix(Map __instance)
+        {
+            Main.MapComponentsInitializing(__instance);
+        }
+    }
+}
---
+++ Mods/RocketMan - Performance Mod/Source/Cosmodrome/Core/Patches/TickManager_Patch.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using Verse;
+
+namespace RocketMan.Patches
+{
+    [RocketStartupPatch(typeof(TickManager), nameof(TickManager.DoSingleTick))]
+    public static class TickManager_DoSingleTick_Patch
+    {
+        private static FieldInfo ftickListNormal = AccessTools.Field(typeof(TickManager), nameof(TickManager.tickListNormal));
+
+        private static MethodBase mTickList_Tick = AccessTools.Method(typeof(TickList), nameof(TickList.Tick));
+
+        private static MethodBase mMain_Tick = AccessTools.Method(typeof(Main), nameof(Main.Tick));
+
+        [HarmonyPriority(int.MaxValue)]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = instructions.ToList();
+            var finished = false;
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                CodeInstruction code = codes[i];
+                if (!finished)
+                {
+                    if (code.opcode == OpCodes.Ldarg_0
+                        && codes[i + 1].opcode == OpCodes.Ldfld
+                        && codes[i + 1].OperandIs(ftickListNormal)
+                        && codes[i + 2].opcode == OpCodes.Callvirt
+                        && codes[i + 2].OperandIs(mTickList_Tick))
+                    {
+                        finished = true;
+                        yield return new CodeInstruction(OpCodes.Call, mMain_Tick) { labels = code.labels };
+                        code.labels = new List<Label>();
+                    }
+                }
+                yield return code;
+            }
+        }
+    }
+
+    [RocketPatch(typeof(TickManager), nameof(TickManager.DoSingleTick))]
+    public static class TickManager_DoSingleTick_Context_Patch
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [HarmonyPriority(Priority.First)]
+        public static void Prefix()
+        {
+            RocketStates.Context = ContextFlag.Ticking;
+        }
+    }
+
+    [RocketPatch(typeof(TickManager), nameof(TickManager.TickRateMultiplier), MethodType.Getter)]
+    public static class TickManager_TickRateMultiplier_Patch
+    {
+        public static bool Prepare() => !RocketCompatibilityInfo.SmartSpeedLoaded;
+
+        [HarmonyPriority(int.MinValue)]
+        public static bool Prefix(ref float __result)
+        {
+            if (true
+                && RocketPrefs.Enabled
+                && RocketDebugPrefs.Debug150MTPS
+                && RocketDebugPrefs.Debug)
+            {
+                __result = 150f;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [RocketPatch(typeof(TickManager), nameof(TickManager.Notify_GeneratedPotentiallyHostileMap))]
+    public static class TickManager_Notify_GeneratedPotentiallyHostileMap_Patch
+    {
+        public static bool Prepare() => !RocketCompatibilityInfo.SmartSpeedLoaded;
+
+        [HarmonyPriority(int.MinValue)]
+        public static bool Prefix()
+        {
+            if (true
+                && Prefs.DevMode
+                && RocketDebugPrefs.Debug
+                && RocketPrefs.DisableForcedSlowdowns)
+                return false;
+            return true;
+        }
+    }
+
+    [RocketPatch(typeof(TickManager), nameof(TickManager.TickManagerUpdate))]
+    public static class TickManager_TickManagerUpdate_Patch
+    {
+        public static bool Prepare() => !RocketCompatibilityInfo.SmartSpeedLoaded;
+
+        [HarmonyPriority(int.MinValue)]
+        public static bool Prefix(TickManager __instance)
+        {
+            if (RocketPrefs.Enabled && RocketDebugPrefs.Debug && RocketStates.SingleTickIncrement)
+            {
+                if (RocketStates.SingleTickIncrement && RocketStates.SingleTickLeft > 0)
+                {
+                    if (__instance.Paused)
+                    {
+                        __instance.TogglePaused();
+                    }
+                    RocketStates.SingleTickLeft = Math.Max(RocketStates.SingleTickLeft - 1, 0);
+                    return true;
+                }
+                if (!__instance.Paused)
+                {
+                    __instance.Pause();
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+}
